763d7ac97a500efcebd0e145a0e4357f341569ce
Arnav Gupta <arnav@codingblocks.com>
2019-06-09 14:45:19 +0530

    adding code in kotlin
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture02/MyAppKt/app/src/main/java/com/codingblocks/myappkt/MainActivity.kt
+++ Lecture02/MyAppKt/app/src/main/java/com/codingblocks/myappkt/MainActivity.kt
@@ -2,11 +2,19 @@ package com.codingblocks.myappkt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnAdd.setOnClickListener {
+            val a = etVar1.text.toString().toInt()
+            val b = etVar2.text.toString().toInt()
+            val c = (a + b).toString()
+            tvResult.text = c
+        }
     }
 }
