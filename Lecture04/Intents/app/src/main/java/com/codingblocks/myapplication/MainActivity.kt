0cf65d58a65e982b846ace7b788234a224802b39
Arnav Gupta <arnav@codingblocks.com>
2019-06-15 13:14:16 +0530

    activity lifecycle
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture04/Intents/app/src/main/java/com/codingblocks/myapplication/MainActivity.kt
+++ Lecture04/Intents/app/src/main/java/com/codingblocks/myapplication/MainActivity.kt
@@ -3,16 +3,52 @@ package com.codingblocks.myapplication
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val TAG = "MAIN"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Log.d(TAG, "onCreate")
+
         setContentView(R.layout.activity_main)
 
         btnOpenAct.setOnClickListener {
-            startActivity(Intent(this, SecondActivity::class.java))
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnClose.setOnClickListener {
+            finish()
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        Log.d(TAG, "onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d(TAG, "onStop")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy")
+        super.onDestroy()
+    }
+
 }
