fd8fddca7ded3013b96ee3f4554f6e1d861bc666
Arnav Gupta <arnav@codingblocks.com>
2019-07-07 13:49:47 +0530

    external folder file read/write
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture13/FileReadWrite/app/src/main/java/com/codingblocks/filereadwrite/MainActivity.kt
+++ Lecture13/FileReadWrite/app/src/main/java/com/codingblocks/filereadwrite/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.codingblocks.filereadwrite
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.*
 
@@ -12,8 +17,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        ActivityCompat.requestPermissions(this,
+            arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ),
+            111
+        )
+
         Log.d("FILES", "Files dir = ${filesDir.path}")
-        val file = File(filesDir, "myfile.txt")
+
+
+        val extDir = Environment.getExternalStorageDirectory()
+        Log.d("FILES", "External dir = ${extDir.path}")
+
+
+        val file = File(extDir, "myfile.txt")
+
+
 
         btnSave.setOnClickListener {
             // Save data of etData to file
@@ -39,5 +60,22 @@ class MainActivity : AppCompatActivity() {
 //            val br = BufferedReader(isr)
 //            br.readLine()
         }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+
+        if ((grantResults[0] == PackageManager.PERMISSION_DENIED) || (grantResults[1] == PackageManager.PERMISSION_DENIED)) {
+            Toast.makeText(
+                this,
+                "Cannot run without permissions",
+                Toast.LENGTH_SHORT
+            ).show()
+            finish()
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
