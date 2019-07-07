ca9938324b536974c9374e182bdaaa08467a1026
Arnav Gupta <arnav@codingblocks.com>
2019-07-07 13:25:41 +0530

    Save and restore from file
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Lecture13/FileReadWrite/app/src/main/java/com/codingblocks/filereadwrite/MainActivity.kt
+++ Lecture13/FileReadWrite/app/src/main/java/com/codingblocks/filereadwrite/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.File
+import java.io.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,16 +13,31 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Log.d("FILES", "Files dir = ${filesDir.path}")
+        val file = File(filesDir, "myfile.txt")
 
         btnSave.setOnClickListener {
             // Save data of etData to file
 
-            val file = File(filesDir, "myfile.txt")
             Log.d("FILES", "exists = ${file.exists()}")
+//            file.writeText(etData.text.toString())
+            file.appendText(etData.text.toString())
+
+//            val foutStream = FileOutputStream(file)
+//            val bytes = etData.text.toString().toByteArray()
+//            foutStream.write(bytes)
+//            foutStream.close()
         }
 
         btnRestore.setOnClickListener {
             // Restore data from file and show in tvFileText
+            val existingText = file.readText()
+            tvFileText.text = existingText
+
+//            val fis = FileInputStream(file)
+//            val byteArray = ByteArray(20)
+//            val isr = InputStreamReader(fis)
+//            val br = BufferedReader(isr)
+//            br.readLine()
         }
     }
 }
