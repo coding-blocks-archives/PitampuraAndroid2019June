f5f8e6c4fdf31897179c157acc360ef451af35ac
pulkit-mac <aggarwalpulkit596@gmail.com>
2019-07-11 14:16:03 +0530

    Todo using Adapter

---
+++ Lecture14/NotesUsingRoom/app/src/main/java/com/example/roomnotes/ListItemClickListener.kt
@@ -0,0 +1,5 @@
+package com.example.roomnotes
+
+interface ListItemClickListener{
+    fun lisitemClick(task:Todo,position:Int)
+}
--- Lecture14/NotesUsingRoom/app/src/main/java/com/example/roomnotes/MainActivity.kt
+++ Lecture14/NotesUsingRoom/app/src/main/java/com/example/roomnotes/MainActivity.kt
@@ -4,19 +4,51 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val db = Room.databaseBuilder(
+    val db: AppDatabase by lazy {
+        Room.databaseBuilder(
             this,
             AppDatabase::class.java,
             "todo.db"
         ).allowMainThreadQueries()
             .fallbackToDestructiveMigration()
             .build()
+    }
+
+    var list = arrayListOf<Todo>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        list = db.todoDao().getAllTodo() as ArrayList<Todo>
+
+        val adapter = TaskAdapter(list)
+
+        adapter.listItemClickListener = object : ListItemClickListener {
+            override fun lisitemClick(task: Todo, position: Int) {
+                db.todoDao().delete(task)
+            }
+
+        }
+
+        lvTodolist.adapter = adapter
+
+
+        btnAdd.setOnClickListener {
+
+            db.todoDao().insertRow(
+                Todo(
+                    task = etNewItem.text.toString(),
+                    status = false
+                )
+            )
+            list = db.todoDao().getAllTodo() as ArrayList<Todo>
+            adapter.updateTasks(list)
+        }
+
+
     }
 }
