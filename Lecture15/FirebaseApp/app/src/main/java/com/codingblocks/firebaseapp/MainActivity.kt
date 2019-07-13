359ddb9a3fd15fef8781afc375dedcba23d8bd31
pulkit-mac <aggarwalpulkit596@gmail.com>
2019-07-13 14:52:01 +0530

    Auth using Phone

--- Lecture15/FirebaseApp/app/src/main/java/com/codingblocks/firebaseapp/MainActivity.kt
+++ Lecture15/FirebaseApp/app/src/main/java/com/codingblocks/firebaseapp/MainActivity.kt
@@ -3,9 +3,14 @@ package com.codingblocks.firebaseapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.firebase.FirebaseException
 import com.google.firebase.FirebaseExceptionMapper
+import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthProvider
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,32 +21,72 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        if (auth.currentUser != null) {
-            button.setOnClickListener {
-                auth.signInWithEmailAndPassword(edtvEail.text.toString(), edtvPass.text.toString())
-                    .addOnCompleteListener {
-                        button.isEnabled = false
-                    }.addOnSuccessListener {
-                        button.isEnabled = true
-                        Toast.makeText(this, "Signed In Successfully", Toast.LENGTH_SHORT).show()
-                    }.addOnFailureListener {
-                        Toast.makeText(this, it.localizedMessage, Toast.LENGTH_SHORT).show()
-                    }
+        val callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            override fun onVerificationCompleted(p0: PhoneAuthCredential?) {
+                Toast.makeText(this@MainActivity, "Verification Completed", Toast.LENGTH_LONG).show()
+                signUpWithPhone(p0)
             }
-        } else {
 
-            button.setOnClickListener {
-                auth.createUserWithEmailAndPassword(edtvEail.text.toString(), edtvPass.text.toString())
-                    .addOnCompleteListener {
-                        button.isEnabled = false
-                    }.addOnSuccessListener {
-                        button.isEnabled = true
-                        Toast.makeText(this, "Created Account Successfully", Toast.LENGTH_SHORT).show()
-                    }.addOnFailureListener {
-                        Toast.makeText(this, it.localizedMessage, Toast.LENGTH_SHORT).show()
-                    }
+            override fun onVerificationFailed(p0: FirebaseException?) {
+                Toast.makeText(this@MainActivity, "Exception Completed ${p0?.localizedMessage}", Toast.LENGTH_LONG)
+                    .show()
             }
+
+            override fun onCodeSent(p0: String?, p1: PhoneAuthProvider.ForceResendingToken?) {
+                super.onCodeSent(p0, p1)
+                Toast.makeText(this@MainActivity, "Code Sent Completed", Toast.LENGTH_LONG).show()
+
+            }
+
         }
+
+//        if (auth.currentUser != null) {
+////            button.setOnClickListener {
+////                auth.signInWithEmailAndPassword(edtvEail.text.toString(), edtvPass.text.toString())
+////                    .addOnCompleteListener {
+////                        button.isEnabled = false
+////                    }.addOnSuccessListener {
+////                        button.isEnabled = true
+////                        Toast.makeText(this, "Signed In Successfully", Toast.LENGTH_SHORT).show()
+////                    }.addOnFailureListener {
+////                        Toast.makeText(this, it.localizedMessage, Toast.LENGTH_SHORT).show()
+////                    }
+////            }
+////        } else {
+////
+////            button.setOnClickListener {
+////                auth.createUserWithEmailAndPassword(edtvEail.text.toString(), edtvPass.text.toString())
+////                    .addOnCompleteListener {
+////                        button.isEnabled = false
+////                    }.addOnSuccessListener {
+////                        button.isEnabled = true
+////                        Toast.makeText(this, "Created Account Successfully", Toast.LENGTH_SHORT).show()
+////                    }.addOnFailureListener {
+////                        Toast.makeText(this, it.localizedMessage, Toast.LENGTH_SHORT).show()
+////                    }
+//            }
+//        }
+
+        button.setOnClickListener {
+            PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                "+91${edtvEail.text}", // Phone number to verify
+                60, // Timeout duration
+                TimeUnit.SECONDS, // Unit of timeout
+                this, // Activity (for callback binding)
+                callbacks
+            ) // OnVerificationStateChangedCallbacks
+        }
+    }
+
+    private fun signUpWithPhone(p0: PhoneAuthCredential?) {
+        auth.signInWithCredential(p0 as AuthCredential)
+            .addOnCompleteListener {
+
+            }.addOnSuccessListener {
+
+            }.addOnFailureListener {
+
+            }
+
     }
 }
