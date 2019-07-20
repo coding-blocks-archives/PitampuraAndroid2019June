89c02e67ad08c894957fd4fbc075fb94445b42fd
pulkit-mac <aggarwalpulkit596@gmail.com>
2019-07-20 13:35:15 +0530

    ADD FILES

---
+++ Lecture18/BroadcastRecievers/app/src/main/java/com/codingblocks/broadcastrecievers/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.codingblocks.broadcastrecievers
+
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val intentFilter = IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION).apply {
+            addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+        }
+        registerReceiver(CallReciever(), intentFilter)
+    }
+}
